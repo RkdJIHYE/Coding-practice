51778a6b04bd42ab08c831bc9ff2aa36419adbca
RkdJIHYE <kangbs210210@gmail.com>
2025-09-17 22:30:41 +0900

    Feat:이진탐색

---
+++ 이진탐색/백준[1920] 복사본/Baekjoon1920.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n = input()
+list_A = list(map(int,input().split()))
+
+m = input()
+list_B = list(map(int,input().split()))
+
+
+print(list_A)
+print(list_B)
