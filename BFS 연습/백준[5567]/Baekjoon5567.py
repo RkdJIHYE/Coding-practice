c9b94bee95a79b1484694b157c97cedfc79398ae
RkdJIHYE <kangbs210210@gmail.com>
2025-09-20 22:56:20 +0900

    Feat:BFS백준추가

--- BFS 연습/백준[5567]/Baekjoon5567.py
+++ BFS 연습/백준[5567]/Baekjoon5567.py
@@ -1,29 +1,29 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
-n = int(input().strip())
-m = int(input().strip())
+n = int(input())
+m = int(input())
 
-map = [list(map(int,input().split())) for _ in range(m)]
+f= [[] for _ in range(n+1)]
+
+for i in range(m):
+    a,b = map(int,input().split())
+    f[a].append(b)
+    f[b].append(a)
 
 chk = [False]*(n+1)
+fone = f[1]
 
-def bfs(y,x):
-    q=[(y,x)]
-    while q:
-        d
-        
-    return 0 
-        
-fr = []
+if not fone:
+    print(0)
+    
 
-while fr:     
-    for j in range (m):
-        if map[j][0]==1 and chk[(map[j][1])]==False:
-            chk[(map[j][1])]==True
-            fr.append(map[j][1])
-        
-        
-    
-    
-print(sum(chk))
+else:
+    for i in fone:
+        if chk[i]==False:
+            chk[i]=True
+            for next_edge in f[i]:
+                if chk[next_edge]==False:
+                    chk[next_edge]=True
+    print(sum(chk))
