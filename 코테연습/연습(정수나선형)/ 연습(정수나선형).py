d05594e382caa8318c9d0c17a6e6b8c2de87bb6c
RkdJIHYE <kangbs210210@gmail.com>
2025-09-26 23:12:10 +0900

    Feat:정수나선형

---
+++ 코테연습/연습(정수나선형)/ 연습(정수나선형).py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = [[0]*n for _ in range(n)]
+    chk = [[False]*n for _ in range(n)]
+    
+    #오 아래 왼 위 순서로 반복된다.
+    dy = [0,-1,0,1]
+    dx = [1,0,-1,0]
+    
+    
+    
+
+    return answer
