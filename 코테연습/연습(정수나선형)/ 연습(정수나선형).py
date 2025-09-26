adb7bf00ccd1c30ac6b4c8831159c10ec5176279
RkdJIHYE <kangbs210210@gmail.com>
2025-09-26 23:48:11 +0900

    Fix:정수 나선형 수정

--- 코테연습/연습(정수나선형)/ 연습(정수나선형).py
+++ 코테연습/연습(정수나선형)/ 연습(정수나선형).py
@@ -1,12 +1,33 @@
 def solution(n):
-    answer = [[0]*n for _ in range(n)]
-    chk = [[False]*n for _ in range(n)]
-    
-    #오 아래 왼 위 순서로 반복된다.
-    dy = [0,-1,0,1]
-    dx = [1,0,-1,0]
-    
-    
-    
+    if n <= 0:
+        return []
+
+    answer = [[0] * n for _ in range(n)]
+    visited = [[False] * n for _ in range(n)]
+
+    # 오른쪽, 아래, 왼쪽, 위 (시계 방향)
+    dy = [0, 1, 0, -1]
+    dx = [1, 0, -1, 0]
+
+    y, x = 0, 0
+    dir_idx = 0
+    k = 1
+    max_k = n * n
+
+    while k <= max_k:
+        answer[y][x] = k
+        visited[y][x] = True
+        k += 1
+
+        ny = y + dy[dir_idx]
+        nx = x + dx[dir_idx]
+
+        # 다음 칸이 유효하고 미방문이면 이동, 아니면 방향 바꾸기
+        if 0 <= ny < n and 0 <= nx < n and not visited[ny][nx]:
+            y, x = ny, nx
+        else:
+            dir_idx = (dir_idx + 1) % 4
+            y += dy[dir_idx]
+            x += dx[dir_idx]
 
     return answer
