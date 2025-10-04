48b483c26842bf2cd68e1955769560e9d1f77afa
RkdJIHYE <kangbs210210@gmail.com>
2025-10-04 23:55:24 +0900

    Fix : 튜플

--- 코테연습/튜플/튜플.py
+++ 코테연습/튜플/튜플.py
@@ -3,9 +3,11 @@ def solution(s):
     for i in s.split("},"):
         li.append(i.replace("{","").replace("}","").split(","))
     li.sort(key=len)
+    
     answer=[]
     for i in li:
         for j in i:
             if j not in answer:
                 answer.append(j)
+                
     return list(map(int,answer))
