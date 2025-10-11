7306b4bcb3545e930be7f04f29325ba313f81aa1
RkdJIHYE <kangbs210210@gmail.com>
2025-10-11 22:51:42 +0900

    Fix:덧칠하기

--- 코테연습/덧칠하기 /덧칠하기.py
+++ 코테연습/덧칠하기 /덧칠하기.py
@@ -1,6 +1,20 @@
 def solution(n, m, section):
     answer = 0
-    max_l = max(section)
-    min_l = min(section)
+    chk = [True]*(n+1)
+    for i in section:
+        chk[i]=False
+
     
+    for j in range(len(chk)):
+        if chk[j]==True:
+            continue
+        else:
+            answer+=1
+            for k in range(m):
+                if j+k<len(chk):
+                    chk[j+k]=True
+                else:
+                    break
+
+        
     return answer
