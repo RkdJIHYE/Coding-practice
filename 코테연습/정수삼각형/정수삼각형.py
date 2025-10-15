5298b68af606eb4cf3075290965c7f6c9ab256c5
RkdJIHYE <kangbs210210@gmail.com>
2025-10-15 23:17:21 +0900

    Fix:정수삼각형

--- 코테연습/정수삼각형/정수삼각형.py
+++ 코테연습/정수삼각형/정수삼각형.py
@@ -1,3 +1,9 @@
 def solution(triangle):
     answer = 0
+    for i in range(1,len(triangle)):
+        for j in range(len(triangle[i])):
+            triangle[i][j]+=triangle[i-1][j-1]
+                       
+                       
+    print(triangle)
     return answer
