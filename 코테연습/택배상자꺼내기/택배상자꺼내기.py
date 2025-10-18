b38926c9823035d008ab8d642c819cca563eb227
RkdJIHYE <kangbs210210@gmail.com>
2025-10-18 21:59:24 +0900

    Fix:택배상자꺼내기

--- 코테연습/택배상자꺼내기/택배상자꺼내기.py
+++ 코테연습/택배상자꺼내기/택배상자꺼내기.py
@@ -3,8 +3,7 @@ def solution(n, w, num):
     # w 가로 길이
     # num 꺼내고싶은 상자 번호
     answer = 0
-    #좌 우 이동을 위한 변수
-    pt = 0
+    
     cnt = n//w +1
     li = [[0]*w for _ in range(cnt)]
     print(li)
@@ -13,22 +12,36 @@ def solution(n, w, num):
     y=0
     chk = True
     tmp = 1 #1부터 시작
+    #좌 우 이동을 위한 변수
+    
     for i in range(len(li)-1,-1,-1):
-        if chk == True:  
+        if chk == True:
+            pt = 0
+            chk = False
             for j in range(len(li[i])):
-                chk = False
+                if tmp == num :
+                    y=i
+                    x=pt
                 li[i][pt]=tmp
                 tmp+=1
                 pt+=1
                 if tmp >n:
                     break
         elif chk == False:
+            pt = w-1
+            chk = True
             for j in range(len(li[i])):
-                chk = True
+                if tmp == num :
+                    y=i
+                    x=pt
                 li[i][pt]=tmp
                 tmp+=1
                 pt-=1
                 if tmp >n:
                     break
-    print(li)
+                    
+    if li[0][x]!=0:
+        answer=y+1
+    else:
+        answer=y
     return answer
