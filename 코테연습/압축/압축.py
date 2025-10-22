42f21529306a5a24711a77aca8c24d3a019cfe71
RkdJIHYE <kangbs210210@gmail.com>
2025-10-22 13:51:18 +0900

    Fix:압축

--- 코테연습/압축/압축.py
+++ 코테연습/압축/압축.py
@@ -1,3 +1,23 @@
 def solution(msg):
     answer = []
+    li = ['0','A','B','C','D','E','F','G','H','I','J','K',
+          'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    
+    i = 0
+    while i < len(msg):
+        # 현재 위치에서 가장 긴 문자열 w 찾기
+        w = msg[i]
+        while i + len(w) < len(msg) and msg[i:i+len(w)+1] in li:
+            w = msg[i:i+len(w)+1]
+        
+        # 현재 w의 인덱스 추가
+        answer.append(li.index(w))
+        
+        # 사전에 새로운 단어 추가 (w + 다음 문자)
+        if i + len(w) < len(msg):
+            li.append(w + msg[i + len(w)])
+        
+        # 다음 위치로 이동
+        i += len(w)
+    
     return answer
